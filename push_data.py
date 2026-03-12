f83a02b0106a51d2a9261ec0c9f7eb97bb175da9
Satyam Tiwari <Satyam.Tiwari@ess.com>
2026-03-12 18:08:35 +0530

    Mongo DB Setup Done

---
+++ push_data.py
@@ -0,0 +1,13 @@
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://satyam123:Ess123@cluster0.ushxwal.mongodb.net/?appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
